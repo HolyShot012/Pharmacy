7ec832861760b82fab4ebec66071b126ce896a9f
duc Tri <ductri02lt2@gmail.com>
2025-07-30 16:31:23 +0700

    [pu

--- backend/pharmacy/api/urls.py
+++ backend/pharmacy/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from .views import user_list
+from .views import user_list, create_user
 urlpatterns = [
     path('users/', user_list, name ='user-list'),
-    
+    path('users/create/', create_user, name='create-user'),
 ]
--- backend/pharmacy/api/views.py
+++ backend/pharmacy/api/views.py
@@ -12,3 +12,11 @@ def user_list(request):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
