c9baf8e302135a0eeae771b7eead2ca631463c30
Polkanpop <kanniepop@gmail.com>
2025-08-05 17:41:49 +0700

    push

--- frontend/native/app/(tabs)/products.jsx
+++ frontend/native/app/(tabs)/products.jsx
@@ -35,10 +35,10 @@ const ProductPage = () => {
   const itemsPerPage = 10;
 
   const categories = [
-    { id: 1, name: 'Medicines', icon: '💊', color: '#fee2e2' },
+    { id: 1, name: 'Medicine', icon: '💊', color: '#fee2e2' },
     { id: 2, name: 'Medical Equipment', icon: '🩺', color: '#d1fae5' },
     { id: 3, name: 'Cosmetic', icon: '🧴', color: '#dbeafe' },
-    { id: 4, name: 'Selfcare', icon: '🧘‍♀️', color: '#fce7f3' },
+    { id: 4, name: 'Supplement', icon: '🧘‍♀️', color: '#fce7f3' },
   ];
 
   // Handle URL parameters to set initial category
